e0c0d95667b0b1668c744e23497f43ccc026a6d3
devops <jed0809@cwc.edu>
2025-03-06 19:24:17 -0700

    fixed calculator

--- calculator.py
+++ calculator.py
@@ -1,10 +1,10 @@
 import math
 
-num1 = input("Please provide 1st number:")
-num2 = input("Please provide 2nd number:")
-
 def calc(x,y):
     result = int(x) * int(y)
     return result
     
-print(calc(num1, num2))
+if __name__ == '__main__':
+    num1 = input("Please provide 1st number:")
+    num2 = input("Please provide 2nd number:")
+    print(calc(num1, num2))
